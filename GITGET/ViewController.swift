604efc1633c791ddc9659f98142381b1fb8cccbb
KyleBHornberger <kylebhornberger@gmail.com>
2016-10-18 10:09:36 -0600

    Initial Commit

---
+++ GITGET/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GITGET
+//
+//  Created by Kyle Hornberger on 10/18/16.
+//  Copyright © 2016 Kyle Hornberger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
